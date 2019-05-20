cb6d152a31253f8c76d90f8dc08d918b32c95e81
Dayana <vindasd82@gmail.com>
2019-05-20 17:21:16 -0600

    [add] analisis de sentimientos

--- Tobix Funcional/Tobix/src/org/tec/tobix/logicaIntegracion/silohay.java
+++ Tobix Funcional/Tobix/src/org/tec/tobix/logicaIntegracion/silohay.java
@@ -9,6 +9,8 @@ import java.sql.SQLException;
 import org.json.JSONObject;
 import org.tec.tobix.dao.ObtenerInformacion;
 import org.tec.tobix.util.Calendario;
+import org.tec.tobix.logicaIntegracion.ToneAnalyzerService;
+import org.tec.tobix.logicaNegocio.Actividad;
 
 import com.ibm.db2.jcc.am.ResultSet;
 import com.ibm.watson.developer_cloud.assistant.v1.model.MessageResponse;
@@ -16,21 +18,12 @@ import com.ibm.watson.developer_cloud.assistant.v1.model.MessageResponse;
 public class silohay {
 	//static Calendario time = new Calendario();
 	//static ObtenerInformacion data = new ObtenerInformacion();
-	static Watson w = Watson.getSingletonInstance();
 
 
-	public static void main(String[] args){
-	 try {
-	//	System.out.print(w.askWatsonMe("hoy"));
-		 MessageResponse response;
-		 response = w.askWatsonMe("5");
-		 String h =  w.getFecha(response);
-		 System.out.print(h);
-		//System.out.print(w.askWatson("comentar"));
-	 }catch( Exception SQLException ){
-		 System.out.println(SQLException);
-	 }
-		
+
+	public static void main(String[] args) throws SQLException{
+		LanguageTranslatorApp LT= new LanguageTranslatorApp();
+		LT.correr();
 	}
 		
 		
